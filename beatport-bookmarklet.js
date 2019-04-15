1dfc530f7d65e8d81dbd6f07b2d131de73d4bf48
aubford <musicaubrey@gmail.com>
2019-04-15 13:52:51 -0600

    fix beatort

--- beatport-bookmarklet.js
+++ beatport-bookmarklet.js
@@ -1,2 +1,22 @@
 /* eslint-disable */
-javascript:(function(){window.open('https://www.beatport.com/search?q=' + document.getElementsByClassName('current-track')[0].innerText, '_blank')})()
+const testString = 'I Get Deep (feat Roland Clark - Late Nite Tuff Guy Remix/Emanuel Satie Rework)'
+const testStringB = 'Panic by Jamie Jones  — Buy'
+
+
+javascript:(function() {
+  var formatString = str => str
+    .replace('/', ' ')
+    .replace(/[^\w\s]/g, '')
+    .split(' ')
+    .map(wrd => wrd.trim())
+    .filter(wrd => wrd && wrd !== 'feat' && wrd !== 'by' && wrd !== 'Buy')
+    .join('+');
+  window.open(
+    'https://www.beatport.com/search?q=' +
+    formatString(
+    document.getElementsByClassName('current-track')[0].innerText + ' ' +
+    document.getElementsByClassName('current-artist')[0].innerText
+    ),
+    '_blank'
+  );
+})()
