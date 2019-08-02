ae3d358a65f548041f95ef3ca91e0df85c468deb
Aubrey <aubrey@dataprises-mbp.lan>
2019-08-02 12:06:14 -0600

    simplified zshrc implementation

--- zshrc.sh
+++ zshrc.sh
@@ -1,13 +1,108 @@
 #@IgnoreInspection BashAddShebang
 
+
+# what your ~/.zshrc file should look like, simply:
+# source "/Users/aubrey/workspace/aubrey-snippetts/zshrc.sh"
+
+# If you come from bash you might have to change your $PATH.
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# Path to your oh-my-zsh installation.
+export ZSH="/Users/aubrey/.oh-my-zsh"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+ZSH_THEME="robbyrussell"
+
+# Set list of themes to pick from when loading at random
+# Setting this variable when ZSH_THEME=random will cause zsh to load
+# a theme from this variable instead of looking in ~/.oh-my-zsh/themes/
+# If set to an empty array, this variable will have no effect.
+# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+
+# Uncomment the following line to use case-sensitive completion.
+# CASE_SENSITIVE="true"
+
+# Uncomment the following line to use hyphen-insensitive completion.
+# Case-sensitive completion must be off. _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Uncomment the following line to disable bi-weekly auto-update checks.
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment the following line to automatically update without prompting.
+# DISABLE_UPDATE_PROMPT="true"
+
+# Uncomment the following line to change how often to auto-update (in days).
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS=true
+
+# Uncomment the following line to disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+# Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+# HIST_STAMPS="mm/dd/yyyy"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+#////////////////////////////////////////////////////////////////////////////////////
+#////////////////////////////////////////////////////////////////////////////////////
+#////////////////////////////////////////////////////////////////////////////////////
+
 bindkey "\e\eOD" backward-word
 bindkey "\e\eOC" forward-word
 
 # oh-my-zsh non-default settings
 DISABLE_AUTO_TITLE="true"
-ENABLE_CORRECTION="true"
 COMPLETION_WAITING_DOTS="true"
-plugins=(zsh-nvm z wd github npm node osx)
+
+plugins=(node z wd github npm osx zsh-nvm)
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+
+export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+export PATH="/usr/local/opt/mongodb-community@4.0/bin:$PATH"
+
 
 # PUT PATH TO THIS FILE HERE; '-g' aliases can be used anywhere in the command, not just the beginning
 alias -g zrc="~/workspace/aubrey-snippetts/zshrc.sh"
@@ -29,8 +124,6 @@ alias rl='source ~/.zshrc'
 alias genodecide='killall node'
 #get file size
 alias size="du -hs"
-# find the origin of a given command
-alias locate="which"
 # copy global npm packages from given package
 alias nvm-cp="nvm copy-packages" #(e.g. 0.10.23)
 # open npm package in node_modules
@@ -144,6 +237,8 @@ alias psqlog="cat /usr/local/var/postgres/server.log"
 alias rollback='knex migrate:rollback --env development'
 alias migrate='knex migrate:latest --env development'
 alias seed='knex seed:run --env development'
+# run mongo
+alias mongo='mongod --config /usr/local/etc/mongod.conf'
 # ------- Github -----------------------------------------------------------------------------------------------------
 gh() {
   giturl=$(git config --get remote.origin.url)
