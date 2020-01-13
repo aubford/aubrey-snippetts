e56a1949810f9be33d039c4011b4e1a165f96e00
aubreyford <musicaubrey@gmail.com>
2020-01-13 08:00:29 -0700

    add

---
+++ daily-backups.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+AUBREY="aubreyford"
+path+="/usr/local/mysql/bin"
+JDK_HOME="jdk1.8.0_231.jdk"
+METEOR_VERSION_DIR="1.8.3"
+
+source "/Users/aubreyford/workspace/UTIL/aubrey-snippetts/zshrc.sh";
+daily_backup;
