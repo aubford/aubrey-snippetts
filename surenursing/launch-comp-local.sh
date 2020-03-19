5d4335f4b1304c2abcc1d87a209c7b31a30693c5
aubford <musicaubrey@gmail.com>
2020-03-18 18:17:51 -0700

    dont open browser on launch-comp-local

--- surenursing/launch-comp-local.sh
+++ surenursing/launch-comp-local.sh
@@ -6,6 +6,5 @@ export MONGO_URL="mongodb://localhost:27017/local-suredb"
 export METEOR_OFFLINE_CATALOG=1
 #export METEOR_PROFILE=1
 
-open "http://localhost:4000/login"
 meteor --settings settings.json --no-release-check --no-lint --allow-incompatible-update --port 4000
 
